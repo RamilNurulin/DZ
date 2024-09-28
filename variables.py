ad1c9c642a6e1e7ce65d21958b6bbcba9b709593
Ramil <nurullinramil1@gmail.com>
2024-09-28 14:54:50 +0300

    28/09/24

--- variables.py
+++ variables.py
@@ -1,7 +1,7 @@
-number_of_tasks: int = 12
-number_of_hours: float = 1.5
+number_of_tasks = 12
+number_of_hours = 1.5
 course = "Python,"
-time_per_job: float = number_of_hours / number_of_tasks
+time_per_job = number_of_hours / number_of_tasks
 
-print("Курс:", course, "всего задач:", number_of_tasks, ', ' "затрачено часов:",
-      number_of_hours, ' ,', "среднее время выполнения:", time_per_job, ", часа.")
+print("Курс:", course, "всего задач:", number_of_tasks, ',', "затрачено часов:",
+      number_of_hours, ',', "среднее время выполнения:", time_per_job, ", часа.")
